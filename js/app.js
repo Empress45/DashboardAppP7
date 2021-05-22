22df4f9e7df8894260ce73c4daf5a34ed972941a
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2021-05-21 20:54:43 -0400

    adding code

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const alertBanner = document.getElementById("alert");
+
+// create the HTMl for the banner
+alertBanner.innerHTML = 
+    <div>
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">x</p>
+    </div>
