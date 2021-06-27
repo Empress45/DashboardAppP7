3e43d4be59a215838babaa432035faf443e28eda
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2021-06-27 14:45:06 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
 const alertBanner = document.getElementById("alert");
 
 // create the HTMl for the banner
-alertBanner.innerHTML = 
+alertBanner.innerHTML =
     <div>
         <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
         <p class="alert-banner-close">x</p>
     </div>
 
 
-alertBanner.addEventListener('click' , e => {
+alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
         alert.style.display = "none"
     }
-})
+});
 
 // Alert Notifications 
 
-const bellNotification = document.getElementsByClassName("bell-icon")
-const 
+const bellNotification = document.getElementsByClassName("bell-icon");
+const headerAlert = document.getElementById("header-alert");
+const greenDot = document.getElementById("green-dot");
+const closeBtn = document.getElementById("close-btn");
+
+bellNotification.addEventListener('click', () => {
+    if(headerAlert.style.display = 'none') {
+        headerAlert.style.display = 'block';
+    }
+    greenDot.style.display = 'none';
+})
+
+closeBtn.addEventListener('click', () => {
+    headerAlert.style.display = 'none';
+});
