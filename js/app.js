eafc8c388f858b1678003746f0a6ecff63932fba
Patricia <knots1994@yahoo.com>
2021-08-12 15:14:10 -0400

    change

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ const greenDot = document.getElementById("green-dot");
 const closeBtn = document.getElementById("close-btn");
 
 bellNotification.addEventListener('click', () => {
-    if(headerAlert.style.display = 'none') {
+    if (headerAlert.style.display = 'none') {
         headerAlert.style.display = 'block';
     }
     greenDot.style.display = 'none';
@@ -32,3 +32,5 @@ bellNotification.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     headerAlert.style.display = 'none';
 });
+
+const profile = document.getElementsByClassName("profile");
